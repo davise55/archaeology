2147d518a7a72d8c8af19bfa195dfec1e12549fc
Chris Nootenboom <64655634+chrisnootenboom@users.noreply.github.com>
2025-05-07 15:03:57 -0400

    Added julia circuitscape script

--- src/circuitscape_runs.jl
+++ src/circuitscape_runs.jl
@@ -1,5 +1,28 @@
+using Circuitscape
+
+circuitscape_dir = "C:\\Users\\lizad\\OneDrive\\Desktop\\Brown\\Dissertation\\GIS\\circuitscape"
+directions = ["we", "ew", "ns", "sn"]
+
 # Iterate through folders
+for tile_folder in filter(isdir, readdir(circuitscape_dir, join=true))
+    # Get the name of the folder
+    tile_name = basename(tile_folder)
+    println("Processing tile: $tile_name")
 
-# Iterate through direcions (we,ew, etc)
+    # Iterate through directions
+    for direction in directions
+        println("Processing direction: $direction")
+        # Construct the path to the input file
+        ini_file = joinpath(tile_folder, "$direction.ini")
+        println("Input file: $ini_file")
 
-# Run circuitscape
+        # Check if the input file exists
+        if isfile(ini_file)
+            # Run Circuitscape
+            Circuitscape.compute(ini_file)
+        else
+            println("Input file does not exist: $ini_file")
+        end
+    end
+end
+
